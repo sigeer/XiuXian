7e1929328cef378b599672c3f50007a384f68305
sigeer <1452069418@qq.com>
2023-09-12 15:46:13 +0800

    ci: 回滚，定位错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [vue()],
-  server: {
-    // 在 devServer 配置中添加 headers
-    // 关闭 'interest-cohort'
-    headers: {
-      'Permissions-Policy': 'interest-cohort=()',
-    },
-  },
+  plugins: [vue()]
 })
